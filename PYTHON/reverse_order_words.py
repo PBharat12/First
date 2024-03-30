e7f67e2c9de2c2bd741daa25f8464e4aadd6ec5a
PBharat12 <163330437+PBharat12@users.noreply.github.com>
2024-03-30 12:05:16 +0530

    Add files via upload

---
+++ PYTHON/reverse_order_words.py
@@ -0,0 +1,13 @@
+'Program to reverse order of words.'
+s=input("Enter a string:")
+l=s.split()
+l1=[]
+i=len(l)-1
+while i!=-1:
+    l1.append(l[i])
+    i=i-1
+r=' '.join(l1)
+print(r)
+
+    
+    
